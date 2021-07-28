885896f99ddfc9e5bc1a700aa847af376169a95f
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2021-07-28 15:56:51 -0400

    extracted simulator class from fieldfunc

--- fieldopt/objective.py
+++ fieldopt/objective.py
@@ -11,11 +11,15 @@ import scipy.sparse as sparse
 
 from simnibs import cond
 from simnibs.msh import mesh_io
-from simnibs.simulation.fem import FEMSystem, calc_fields
+from simnibs.simulation.fem import FEMSystem, calc_fields, grad_matrix
 import simnibs.simulation.coil_numpy as coil_lib
 from fieldopt.solver_wrapper import get_solver
 
 logger = logging.getLogger(__name__)
+
+FIELD_ENTITY = (3, 2)
+PIAL_ENTITY = (2, 1002)
+FIELDS = ['e']
 
 
 class FieldFunc():
@@ -31,11 +35,6 @@ class FieldFunc():
             surface transformations (use file paths?)
 
     '''
-
-    FIELD_ENTITY = (3, 2)
-    PIAL_ENTITY = (2, 1002)
-    FIELDS = ['e']
-
     def __init__(self,
                  head_model,
                  sampling_domain,
@@ -67,18 +66,12 @@ class FieldFunc():
         self.model = head_model
         self.domain = sampling_domain
         self.tw = tet_weights
-        self.coil = coil
-        self.didt = didt
 
         # Control for coil file-type and SimNIBS changing convention
-        self.normflip = self.coil.endswith('.nii.gz')
+        self.normflip = coil.endswith('.nii.gz')
 
-        self.FEMSystem = None
-        self.initialize(solver)
-        self.num_workers = nworkers
-
-        if nthreads:
-            self.set_num_threads(nthreads)
+        self.simulator = _Simulator(self.mesh, solver, didt, coil,
+                                    np.where(tet_weights), nworkers, nthreads)
 
     def __repr__(self):
         '''
@@ -86,133 +79,18 @@ class FieldFunc():
         '''
 
         print('Mesh:', self.model.mesh)
-        print('Coil:', self.coil)
         return ''
-
-    def __getstate__(self):
-        state = {}
-        state["didt"] = self.didt
-        state["model"] = self.model
-        state["coil"] = self.coil
-        state["FEMSystem"] = self.FEMSystem
-        state["cond"] = self.cond
-        state["tw"] = self.tw
-        return state
 
     @property
     def mesh(self):
         return self.model.mesh
-
-    def set_num_threads(self, mkl_threads):
-
-        if mkl_threads > mp.cpu_count() // 2:
-            logger.warning("Maximum number of cores requested "
-                           " exceeds number of physical cores!")
-            logger.warning(f"Physical Cores: {mp.cpu_count()}")
-            logger.warning(f"Maximum usable cores: {mkl_threads}")
-            logger.warning("Using all cores")
-        else:
-            logger.info("Setting maximum number of usable physical cores"
-                        f"to {mkl_threads}")
-            mkl.set_dynamic(0)
-            mkl.set_num_threads(mkl_threads)
-
-    def initialize(self, solver):
-        '''
-        Initialize objective function to allow for objective
-        function evaluations.
-
-        Caches:
-            - Conductivity list
-            - FEMSystem
-
-        Opens multiprocessing pool to have jobs submitted
-        '''
-
-        logger.info('Storing standard conductivity values...')
-        condlist = [c.value for c in cond.standard_cond()]
-        self.cond = cond.cond2elmdata(self.model.mesh, condlist)
-        logger.info('Successfully stored conductivity values...')
-
-        logger.info("Initializing FEM problem")
-        self.FEMSystem = FEMSystem.tms(self.model.mesh, self.cond)
-        logger.info("Completed FEM initialization")
-
-        logger.info("Computing A matrix of AX=B")
-        A = sparse.csc_matrix(self.FEMSystem.A)
-        A.sort_indices()
-        dof_map = copy.deepcopy(self.FEMSystem.dof_map)
-        if self.FEMSystem.dirichlet is not None:
-            A, _ = self.FEMSystem.dirichlet.apply_to_matrix(A, dof_map)
-        logger.info("Preparing solver...")
-        self.solver = get_solver(solver, A)
-        logger.info("Completed initialization successfully!")
-        return
-
-    def _prepare_tms_matrix(self, m):
-        '''
-        Construct right hand side of the TMS linear
-        problem
-        '''
-
-        # Compute dA/dt for coil posiion on mesh
-        dadt = coil_lib.set_up_tms(self.mesh, self.coil, m, self.didt)
-        dof_map = copy.deepcopy(self.FEMSystem.dof_map)
-
-        # Assemble b
-        b = self.FEMSystem.assemble_tms_rhs(dadt)
-        b, dof_map = self.FEMSystem.dirichlet.apply_to_rhs(
-            self.FEMSystem.A, b, dof_map)
-        return b, dof_map
-
-    def _run_simulation(self, matsimnibs, out):
-
-        if not isinstance(matsimnibs, list):
-            matsimnibs = [matsimnibs]
-
-        if out:
-            out_geos = [f"{out}_coil_{i}.geo" for i in range(len(matsimnibs))]
-            out_sims = [
-                f"{out}_fields_{i}.msh" for i in range(len(matsimnibs))
-            ]
-        else:
-            out_geos = [None] * len(matsimnibs)
-            out_sims = [None] * len(matsimnibs)
-
-        # Only this part needs to be multiproc'd
-        logger.info('Constructing right-hand side of FEM AX=B...')
-        start = time.time()
-        with mp.Pool(processes=self.num_workers) as pool:
-            res = pool.map(self._prepare_tms_matrix, matsimnibs)
-            end = time.time()
-            logger.info(f"Took {end-start:.2f}")
-
-            # Each column vector is a TMS coil position
-            bs = []
-            dofs = []
-            for b, d in res:
-                bs.append(b)
-                dofs.append(d)
-
-            B = np.stack(bs, axis=1)
-            X = self.solver.solve(B).squeeze()
-
-            scores = []
-            for i in range(X.shape[1]):
-                scores.append(
-                    pool.apply_async(self._compute_score, (
-                        X[:, i],
-                        dofs[i],
-                    )))
-            [s.get() for s in scores]
-        return scores
 
     def _compute_score(self, v, dof):
         if self.FEMSystem.dirichlet is not None:
             v, dof_map = self.FEMSystem.dirichlet.apply_to_solution(v, dof)
         V = mesh_io.NodeData(v.squeeze(), name="v", mesh=self.mesh)
         V.mesh = self.mesh
-        out = calc_fields(V, self.FIELDS, cond=self.cond)
+        out = calc_fields(V, FIELDS, cond=self.cond)
         return self._calculate_score(out)
 
     def _calculate_score(self, sim_msh):
@@ -224,7 +102,7 @@ class FieldFunc():
         more complex geometry.
         '''
 
-        tet_ids = self.model.get_tet_ids(self.FIELD_ENTITY[1])
+        tet_ids = self.model.get_tet_ids(FIELD_ENTITY[1])
         normE = sim_msh.elmdata[0].value[tet_ids]
 
         neg_ind = np.where(normE < 0)
@@ -254,7 +132,7 @@ class FieldFunc():
         ]
 
         logger.info('Running simulations...')
-        scores = self._run_simulation(matsimnibs, out_basename)
+        scores = self.simulator.run_simulation(self.mesh, matsimnibs)
         logger.info('Successfully completed simulations!')
 
         return scores
@@ -286,4 +164,126 @@ class FieldFunc():
         n = coil_affine[:3, 2]
         p0 = coil_affine[:3, 3]
         p1 = p0 + (n * 200)
-        return np.linalg.norm(self.model.intercept(p0, p1, self.PIAL_ENTITY))
+        return np.linalg.norm(self.model.intercept(p0, p1, PIAL_ENTITY))
+
+
+class _Simulator:
+    '''
+    Class to manage simulation environment
+    '''
+    def __init__(self,
+                 mesh,
+                 solver,
+                 didt,
+                 coil,
+                 roi=None,
+                 num_workers=None,
+                 nthreads=None):
+
+        self.cond = None
+        self.FEMSystem = None
+        self.solver = None
+        self.didt = didt
+        self.coil = coil
+        self.num_workers = num_workers if num_workers else 1
+        if nthreads:
+            self.set_num_threads(nthreads)
+
+        logger.info('Storing standard conductivity values...')
+        condlist = [c.value for c in cond.standard_cond()]
+        conductivities = cond.cond2elmdata(mesh, condlist)
+        logger.info('Successfully stored conductivity values...')
+
+        logger.info("Initializing FEM problem")
+        self.FEMSystem = FEMSystem.tms(mesh, conductivities)
+        logger.info("Completed FEM initialization")
+
+        logger.info('Constructing ROI')
+        D = grad_matrix(mesh, split=True)
+        gm_vol_mask = np.where((mesh.elm.tag1 == 2) * (mesh.elm.elm_type == 4))
+        if not roi:
+            roi = np.where(np.ones_like(gm_vol_mask[0]))
+        self.D = [d.tocsc()[gm_vol_mask][roi] for d in D]
+        self.cond = conductivities.value[gm_vol_mask][roi]
+
+        logger.info("Computing A matrix of AX=B")
+        A = sparse.csc_matrix(self.FEMSystem.A)
+        A.sort_indices()
+        dof_map = copy.deepcopy(self.FEMSystem.dof_map)
+        if self.FEMSystem.dirichlet is not None:
+            A, _ = self.FEMSystem.dirichlet.apply_to_matrix(A, dof_map)
+        logger.info("Preparing solver...")
+        self.solver = get_solver(solver, A)
+        logger.info("Completed initialization successfully!")
+
+    def __getstate__(self):
+        state = {}
+        state["didt"] = self.didt
+        state["coil"] = self.coil
+        state["FEMSystem"] = self.FEMSystem
+        return state
+
+    def set_num_threads(self, mkl_threads):
+
+        if mkl_threads > mp.cpu_count() // 2:
+            logger.warning("Maximum number of cores requested "
+                           " exceeds number of physical cores!")
+            logger.warning(f"Physical Cores: {mp.cpu_count()}")
+            logger.warning(f"Maximum usable cores: {mkl_threads}")
+            logger.warning("Using all cores")
+        else:
+            logger.info("Setting maximum number of usable physical cores"
+                        f"to {mkl_threads}")
+            mkl.set_dynamic(0)
+            mkl.set_num_threads(mkl_threads)
+
+    def prepare_tms_matrix(self, mesh, m):
+        '''
+        Construct right hand side of the TMS linear
+        problem
+
+        Arguments;
+            mesh                    simnibs.msh.mesh_io.Msh object
+            m                       Matsimnibs matrix
+
+        Returns:
+            b                       RHS of simulation
+            dof_map                 DOF map for boundary conditions
+        '''
+
+        # Compute dA/dt for coil posiion on mesh
+        dadt = coil_lib.set_up_tms(mesh, self.coil, m, self.didt)
+        dof_map = copy.deepcopy(self.FEMSystem.dof_map)
+
+        # Assemble b
+        b = self.FEMSystem.assemble_tms_rhs(dadt)
+        b, dof_map = self.FEMSystem.dirichlet.apply_to_rhs(
+            self.FEMSystem.A, b, dof_map)
+        return b, dof_map
+
+    def run_simulation(self, mesh, matsimnibs):
+
+        if not isinstance(matsimnibs, list):
+            matsimnibs = [matsimnibs]
+
+        # Only this part needs to be multiproc'd
+        logger.info('Constructing right-hand side of FEM AX=B...')
+        start = time.time()
+
+        args = [(mesh, m) for m in matsimnibs]
+        with mp.Pool(processes=self.num_workers) as pool:
+            res = pool.starmap(self.prepare_tms_matrix, args)
+            end = time.time()
+            logger.info(f"Took {end-start:.2f}")
+
+            # Each column vector is a TMS coil position
+            bs = []
+            dofs = []
+            for b, d in res:
+                bs.append(b)
+                dofs.append(d)
+
+            B = np.stack(bs, axis=1)
+            X = self.solver.solve(B).squeeze()
+
+        return X
